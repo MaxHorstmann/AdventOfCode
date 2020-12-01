e0b6f9cb05cce7592ac9c484d3062edc33e2323c
Max Horstmann <MaxHorstmann@users.noreply.github.com>
2020-12-01 23:15:23 +0000

    the code!

---
+++ 2020/1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static int Do()
+        {
+            var hs = new HashSet<int>();
+            foreach (var line in File.ReadLines(@"input.txt"))
+            {
+                var a = int.Parse(line);
+                var b = 2020-a;
+                if (hs.Contains(b)) return a * b;
+                hs.Add(a);
+            }
+            throw new InvalidOperationException("not found");
+        }
+        static void Main(string[] args)
+        {
+            var n = Do();
+            Console.WriteLine(n);
+        }
+    }
+}

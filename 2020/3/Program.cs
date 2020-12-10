72ea9741c1a7fb5a68e36160e32e913c85174876
Max Horstmann <MaxHorstmann@users.noreply.github.com>
2020-12-10 22:43:00 +0000

    Day 3

---
+++ 2020/3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trees = 0;
+            var pos = 0;
+            foreach (var line in File.ReadLines(@"input.txt"))
+            {
+                if (line[pos] == '#') trees++;
+                pos = (pos + 3) % (line.Length);
+            }
+            Console.WriteLine(trees);
+        }
+    }
+}

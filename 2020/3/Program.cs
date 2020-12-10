1372fc3c3d55281e52b55831d070147eb9858e33
Max Horstmann <MaxHorstmann@users.noreply.github.com>
2020-12-10 23:02:29 +0000

    part 2

--- 2020/3/Program.cs
+++ 2020/3/Program.cs
@@ -6,16 +6,47 @@ namespace _3
 {
     class Program
     {
+        class Slope 
+        {
+            public int Right { get; set; }
+            public int Down {get; set;}
+        }
+        // Right 1, down 1.
+        // Right 3, down 1. (This is the slope you already checked.)
+        // Right 5, down 1.
+        // Right 7, down 1.
+        // Right 1, down 2.
+
+        static Slope[] Slopes = new Slope[] {
+            new Slope { Right = 1, Down = 1},
+            new Slope { Right = 3, Down = 1},
+            new Slope { Right = 5, Down = 1},
+            new Slope { Right = 7, Down = 1},
+            new Slope { Right = 1, Down = 2}
+        };
+
+
         static void Main(string[] args)
         {
-            var trees = 0;
-            var pos = 0;
+            var trees = new int[Slopes.Length];
+            var pos = new int[Slopes.Length];
+
+            var row = 0;
             foreach (var line in File.ReadLines(@"input.txt"))
             {
-                if (line[pos] == '#') trees++;
-                pos = (pos + 3) % (line.Length);
+                for (var i=0; i<Slopes.Length;i++)
+                {
+                    var slope = Slopes[i];
+                    if (row % slope.Down > 0) continue;
+                    if (line[pos[i]] == '#') trees[i]++;    
+                    pos[i] = (pos[i] + slope.Right) % line.Length;                                
+                }
+                row++;
             }
-            Console.WriteLine(trees);
+            Console.WriteLine(string.Join(",", trees.Select(m => m.ToString())));
+            var product = trees.Aggregate(1, (x, y) => x * y);
+            Console.WriteLine(product);
+            
         }
     }
 }
